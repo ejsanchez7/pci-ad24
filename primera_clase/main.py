9d84ab30d321974fbc85912e1cb0e6c0e9a155f3
ejsanchez7 <54457453+ejsanchez7@users.noreply.github.com>
2024-08-13 18:46:12 -0600

    Ejercicio de la primera clase

---
+++ primera_clase/main.py
@@ -0,0 +1,10 @@
+# \n significa salto de línea
+'''
+Este es un comentario
+multilínea
+'''
+print("Tecnológico de Monterrey")
+print("\n")
+print("Bienvenidos al curso de PCI")
+print("\n")
+print("\tAtentamente: Erik")
